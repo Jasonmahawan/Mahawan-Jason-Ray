11726cf7a08429407dd93116b4a6fdb36633a1dd
Jasonmahawan <112732989+Jasonmahawan@users.noreply.github.com>
2023-10-24 07:43:05 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong{
+   public static void main(String []args){
+      System.out.println("Andam nako, nga ihalad kanimong tanan");
+      System.out.println("Andam nako, nga magkuyog tas katapusan");
+      System.out.println("Ug way pag lubad, ning akong gugma");
+      System.out.println("Ug di mo lubad, nga kita rang duha");
+   }
+}
